3d0625212d3effea3887a45b6530eee95c440de0
Zhen Fu <fuzhenfz@126.com>
2019-04-13 16:50:13 +0800

    Update usefir1.m

--- usefir1.m
+++ usefir1.m
@@ -1,13 +1,7 @@
 function [h] = usefir1(mode,n,fp,fs,window,sample,r)
-% mode:模式(1--高通; 2--低通; 3--带通; 4--带阻)
-% n:阶数, 加窗的点数为阶数加1
-% fp:高通和低通时指示截止频率, 带通和带阻时指示下限频率
-% fs:带通和带阻时指示上限频率
-% window:加窗(1--矩形窗; 2--三角窗; 3--巴特利特窗; 4--汉明窗;
-%5--汉宁窗; 6--布莱克曼窗; 7--凯泽窗; 8--契比雪夫窗)
-% r代表加chebyshev窗的r值和加kaiser窗时的beta值
-% sample:采样率
-% h:返回设计好的FIR滤波器系数
+% mode:1--HP; 2--LP; 3--BP; 4--BF
+% n:order
+
 if window == 1
     w = boxcar(n+1);
 end
@@ -46,25 +40,3 @@ end
 if mode == 4
     h = fir1(n,[wp,ws],'stop',w);
 end
-
-%% plot
-
-% m = 0:n;
-% subplot(3,1,1);
-% plot(m,h);grid on;
-% title('冲激响应');
-% axis([0 n 1.1*min(h) 1.1*max(h)]);
-% ylabel('h(n)');xlabel('n');
-% freq_response = freqz(h,1);
-% magnitude = 20*log10(abs(freq_response));
-% m = 0:511; f = m*sample/(2*511);
-% subplot(3,1,2);
-% plot(f,magnitude);grid on;
-% title('幅频特性');
-% axis([0 sample/2 1.1*min(magnitude) 1.1*max(magnitude)]);
-% ylabel('f幅值');xlabel('频率');
-% phase = angle(freq_response);
-% subplot(3,1,3);plot(f,phase);grid on;
-% title('相频特性');
-% axis([0 sample/2 1.1*min(phase) 1.1*max(phase)]);
-% ylabel('相位');xlabel('频率');
